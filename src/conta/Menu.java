81d992cda37db7fb19769f52175db4dd9ddadc0d
Leandro Andrade <andradeleandro.contato@gmail.com>
2023-06-06 13:56:30 -0300

    classe abstrata conta e interface repository

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,7 +1,6 @@
 package conta;
 
 import java.util.Scanner;
-import conta.model.Conta;
 import conta.util.Cores;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
@@ -13,14 +12,6 @@ public class Menu {
 
 //      variáveis
 		int opcao;
-
-//		Teste da Classe Conta
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
 
 //		Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
